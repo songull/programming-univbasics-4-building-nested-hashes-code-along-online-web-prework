09403e5296e7076c513f9cefd86abb044e3d580e
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-13 04:20:22 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,5 +1,6 @@
 def first_challenge
-   epic_tragedy = {montague: "boss"
-
+   epic_tragedy = {montague: "boss", occupation: "super", hobbies:["lit", "always"]},
+   different_tragedy = {capulet: "ball", occupation: "never", hobbies: ["moist", "food"]}
+     
    }
 end
